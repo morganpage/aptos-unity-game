1dbe47e106d9248525ef4604c6ef3f6e891e1a16
Morgan Page <morganjpage@gmail.com>
2023-11-14 08:52:39 +0000

    Initial commit

---
+++ Assets/GAME/Scripts/AptosManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The main entry point into the Aptos blockchain.
+/// </summary>
+
+public class AptosManager : MonoBehaviour
+{
+
+}
